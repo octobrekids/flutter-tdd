7062c6dd83f090e764c68ce5f92e010cc5d4f0d7
octobrekids <chnrd.k@gmail.com>
2022-02-10 19:12:53 +0700

    add usecase common abstract class

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_tdd/core/error/failures.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams {}
